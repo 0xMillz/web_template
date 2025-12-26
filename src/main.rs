3d73c0a8ac16eb3d1025ce57fbfbb392832f56c9
0xMillz <37815163+0xMillz@users.noreply.github.com>
2025-12-26 15:57:11 -0600

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct User {
+    id: u64,
+    username: String,
+    password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Database {
+    tasks: HashMap<u64, Task>,
+    users: HashMap<u64, User>,
+}
+
+impl Database {
+    fn new() -> Self {
+        Self {
+            tasks: HashMap::new(),
+            users: HashMap::new(),
+        }
+    }
+
+    fn insert_task(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    }
+
+    fn insert_user(&mut self, user: User) {
+        self.users.insert(user.id, user);
+    }
+}
+
 fn main() {
-    println!("Hello, world!!");
+    println!("Hello, world!!!");
 }
